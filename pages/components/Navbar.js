d3e9df865df2710be728705c37be26be4d1636a5
André Fernandes <andrefersouza@gmail.com>
2023-03-09 16:49:10 -0300

    configurando as imagens da página

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React, { useEffect } from 'react'
 
+import logo from './../../public/images/logo.png'
+
 export default function Navbar() {
 
     useEffect(() => {
@@ -22,7 +24,7 @@ export default function Navbar() {
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
                 <Image 
-                    src='/images/logo.png'
+                    src={logo}
                     width={180}
                     height={60}
                 />
