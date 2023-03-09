896c7662d67f63591c0ce7e66884d530f39d4d0a
André Fernandes <andrefersouza@gmail.com>
2023-03-09 16:58:57 -0300

    configurando as imagens da página

--- pages/components/Products.js
+++ pages/components/Products.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import React from 'react'
 
+import Reuniao from './../../public/images/reuniao1.jpeg'
+
 export default function Products() {
   let salas = [
-    {name: 'Sala de Reunião', image: './../../public/images/reuniao1.jpeg'},
+    {name: 'Sala de Reunião', image: {Reuniao}},
     {name: 'Sala de Compartilhada', image: './../../public/images/estacao2.png'},
     {name: 'Sala de Atendimento Bem-Estar', image: './../../public/images/bemestar.jpeg'},
     {name: 'Sala de Atendimento Privativa', image: './../../public/images/privativa.jpeg'},
