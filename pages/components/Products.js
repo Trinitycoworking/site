2142e113bf0bfd958bdcf46da61252c8c0f7465e
André Fernandes <andrefersouza@gmail.com>
2023-03-27 16:33:23 -0300

    atualização da sessão projetos e contato

--- pages/components/Products.js
+++ pages/components/Products.js
@@ -6,14 +6,15 @@ import Compartilhada from './../../public/images/estacao2.png'
 import BemEstar from './../../public/images/bemestar.jpeg'
 import Privativa from './../../public/images/privativa.jpeg'
 import Ripada from './../../public/images/ripada.jpeg'
+import Link from 'next/link'
 
 export default function Products() {
   let salas = [
-    {name: 'Sala de Reunião', image: Reuniao},
-    {name: 'Sala de Compartilhada', image: Compartilhada},
-    {name: 'Sala de Atendimento Bem-Estar', image: BemEstar},
-    {name: 'Sala de Atendimento Privativa', image: Privativa},
-    {name: 'Sala de Atendimento Porta Ripada', image: Ripada},
+    {name: 'Sala de Reunião', image: Reuniao, hora: "R$45,00", pacote: "R$400,00", diaria: "R$270,00", mensal: "Indisponível"},
+    {name: 'Sala de Compartilhada', image: Compartilhada, hora: "Indisponível", pacote: "Indisponível", diaria: "R$45,00", mensal: "R$360,00"},
+    {name: 'Sala de Atendimento Bem-Estar', image: BemEstar, hora: "R$35,00", pacote: "R$300,00", diaria: "R$190,00", mensal: "R$450,00"},
+    {name: 'Sala de Atendimento Privativa', image: Privativa, hora: "R$35,00", pacote: "R$300,00", diaria: "R$190,00", mensal: "R$450,00"},
+    {name: 'Sala de Atendimento Porta Ripada', image: Ripada, hora: "R$35,00", pacote: "R$300,00", diaria: "R$190,00", mensal: "R$450,00"},
   ]
 
   return (
@@ -28,12 +29,17 @@ export default function Products() {
               height={400}
             />
             <div className='product-description'>
-              <span>{sala.name}</span>
-              <span></span>
+              <h2>{sala.name}</h2>
+              <h4>Valores</h4>
+              <span>Hora: {sala.hora}</span>
+              <span>Pacote 10h: {sala.pacote}</span>
+              <span>Diária: {sala.diaria}</span>
+              <span>Mensal: {sala.mensal}</span>
             </div>
           </div>
         ))}
       </div>
+      <Link href='https://trinitycoworking.github.io/Trinity_Horarios/' target='_blank'>Consulte aqui a disponibilidade das salas</Link>
     </div>
   )
 }
