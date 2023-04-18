2ad02c3e1fa0bd5a1b0550b43b08148827a5d65d
trinitycoworking <estagiario.trinity@hotmail.com>
2023-04-18 10:19:50 -0300

    ajuste do gh-pages

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
    * Tell Next.js where the `public` folder is.
    * Replace `nextjs-github-pages` with your Github repo project name.
    */
-  assetPrefix: isProd ? "/trinitycoworking/" : "",
+  assetPrefix: isProd ? "/site/" : "",
   /**
    * Disable server-based image optimization.
    *
